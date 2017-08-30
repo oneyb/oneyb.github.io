93076d7dd258c2c997c908f0513128d708363a6d
oneyb <zenlines@gmail.com>
2017-08-30 02:25:32 +0200

    sweet little script to take care of most technical crap

---
+++ copy_freelancer-bs.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+
+# git clone https://github.com/BlackrockDigital/startbootstrap-freelancer
+
+# cp -r startbootstrap-freelancer/*/ static/
+
+cp startbootstrap-freelancer/index.html templates/bs_index.html
+
+sed -ri 's:src="([^"]+)":src="{{ url_for("static", filename="\1") }}":' templates/bs_index.html
+sed -ri 's:href="([^h#])([^"]+)":href="{{ url_for("static", filename="\1\2") }}":' templates/bs_index.html

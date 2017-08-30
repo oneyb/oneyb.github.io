f2fd28b33abf15f196a30c4f8bf712926fb65fe1
oneyb <zenlines@gmail.com>
2017-08-30 02:35:43 +0200

    appended license; cleaned up

--- copy_freelancer-bs.sh
+++ copy_freelancer-bs.sh
@@ -1,10 +1,14 @@
 #!/usr/bin/env bash
 
-# git clone https://github.com/BlackrockDigital/startbootstrap-freelancer
+git clone https://github.com/BlackrockDigital/startbootstrap-freelancer
 
-# cp -r startbootstrap-freelancer/*/ static/
+cp -r startbootstrap-freelancer/*/ static/
 
 cp startbootstrap-freelancer/index.html templates/bs_index.html
 
 sed -ri 's:src="([^"]+)":src="{{ url_for("static", filename="\1") }}":' templates/bs_index.html
+
 sed -ri 's:href="([^h#])([^"]+)":href="{{ url_for("static", filename="\1\2") }}":' templates/bs_index.html
+
+echo -e "\nAdditional licenses for including 'startbootstrap-freelancer':\n" >> LICENSE
+cat startbootstrap-freelancer/LICENSE >> LICENSE
